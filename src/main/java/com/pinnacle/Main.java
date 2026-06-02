44c96af38bd4f61ebb839dbeb56c60b2d36fb6fe
Soumyadeep257 <soumyadeepdas257work@gmail.com>
2026-06-02 19:57:44 +0530

    Final fix

--- src/main/java/com/pinnacle/Main.java
+++ src/main/java/com/pinnacle/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Launching Pinnacle Library Dashboard...");
         
-        // This command boots up the JavaFX window defined in LibraryApp.java
         Application.launch(LibraryApp.class, args);
     }
 }
